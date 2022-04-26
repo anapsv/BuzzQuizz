3db4fb66fab9c5d3a9a36fcf3678dff6ce236faa
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-04-26 17:32:42 -0300

    tristeza e decepção

--- js.js
+++ js.js
@@ -3,6 +3,9 @@ const quizzSelecionado = document.querySelector(".quizzSelecionado");
 let quizzes = [];
 let quizz = [];
 let id;
+let string = ``;
+let contarAcerto = 0;
+let contarCliques = 0;
 function receiveServerQuizz() {
   const serverQuizz = axios.get(
     "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes"
@@ -32,8 +35,59 @@ function openSelectedQuizz(response) {
 }
 function magica(response) {
   quizz = response.data;
-  console.log(quizz);
-  console.log(quizz.title);
-  console.log(quizz.questions[0]);
+  string = `
+  <div class="gambiarra1">
+  <img
+    src="${quizz.image}"
+  />
+  <div class="topTitle">${quizz.title}</div>
+</div>
+  `;
+  for (let i = 0; i < quizz.questions.length; i++) {
+    string += `
+  <div class="question">
+    <div class="questionTitle">${quizz.questions[i].title}</div>
+    <div class="gambiarra2">
+    
+    `;
+    for (let j = 0; j < quizz.questions[i].answers.length; j++) {
+      string += `
+       <div class="questions" onclick="teste(this)" id=${quizz.questions[i].answers[j].isCorrectAnswer}>
+       <img src="${quizz.questions[i].answers[j].image}" />
+       <h3 class="black">${quizz.questions[i].answers[j].text}</h3>
+     </div>
+     
+     `;
+    }
+    string += `
+    </div> 
+    </div>
+    `;
+  }
+  quizzSelecionado.innerHTML += string;
+}
+function teste(response) {
+  let divPai = response.parentNode;
+  let nodeList = divPai.querySelectorAll(".questions");
+  console.log(teste);
+  if (response.id === "true") {
+    contarAcerto += 1;
+    contarCliques += 1;
+    response.querySelector("h3").classList.remove("black");
+    response.querySelector("h3").classList.add("green");
+  } else if (response.id === "false") {
+    contarCliques += 1;
+    response.querySelector("h3").classList.remove("black");
+    response.querySelector("h3").classList.add("red");
+  }
+  for (let k = 0; k < nodeList.length; k++) {
+    let teste = nodeList[k];
+    console.log(teste);
+    teste
+      .querySelector(".black")
+      .parentNode.querySelector("img")
+      .classList.add("brilho");
+    teste.querySelector(".black").removeAttribute("onclick");
+  }
 }
 receiveServerQuizz();
